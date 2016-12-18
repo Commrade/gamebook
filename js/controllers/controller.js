c483db313a6bc98af930bab32060bc7a2a6e487f
Commrade <dyloneal@gmail.com>
2016-12-18 17:58:32 -0500

    added first scenario to controller

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,2 +1,28 @@
 function newGame(){
 };
+
+function questionOne(){
+	var name = prompt("What is your name?");
+	var answerOne = prompt("You and your friend Dave \n
+		have just spent the night at a friends party. \n
+		It's starting to getting fairly late and \n
+		you both decide to go crash at your house. \n
+		You're both fairly buzzed by this point and \n
+		determine your best 3 options for getting home. \n
+		Take a shortcut through the woods, call an uber, \n
+		or walk the long way home. \n\n
+		Enter your choice: 'woods', 'uber', or 'walk'").toLowerCase();
+	if(answerOne === "woods"){
+		questionOneOne();
+	}
+	else if(answerOne === "uber"){
+		questionOneTwo();
+	}
+	else if(answerOne === "walk"){
+		questionOneThree();
+	}
+	else{
+		alert("I didn't quite understand that try 'woods', 'uber', or 'walk'.");
+		questionOne();
+	}
+};
