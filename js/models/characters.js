16a671ddddda442d95264f01b4797b80605992a9
Commrade <dyloneal@gmail.com>
2016-12-18 17:38:54 -0500

    Update Dave Model

--- js/models/characters.js
+++ js/models/characters.js
@@ -2,7 +2,14 @@
 function Stranger(name) {
 	this.name = name;
 	this.comments = {};
-}
+};
 
 //This will be your friend in the story
-var dave = {}
+var dave = {
+	taunts = {
+		chicken: "Come on man, don't be such a chicken.";
+		baby: "I'd give you a bottle to suck on, but I'm fresh out. Guess the little baby will just have to use his thumb.";
+		help: "Oh crap, help me!";
+		advise: "Maybe we should just run for it.";
+	}
+};
