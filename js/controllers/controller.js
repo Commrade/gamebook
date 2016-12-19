7ff5dce6f4c076cec94944a11a293126f1090430
Commrade <dyloneal@gmail.com>
2016-12-18 20:33:03 -0500

    add second scenario

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -34,7 +34,7 @@ function questionOneOne(){
 	var answer = prompt("You and Dave turn on some flashlights\n"+
 		"and begin walking through the woods.\n"+
 		"About half way into the woods you begin hearing the\n"+
-		"sounds of leaves and swigs crunching and footsteps\n"+
+		"sounds of leaves and twigs crunching and footsteps\n"+
 		"approaching your location. You both turn off your lights\n"+
 		"walk a bit further ahead then hide behind some trees.\n"+
 		"You peak your head out for a brief moment to see two\n"+
@@ -120,7 +120,7 @@ function questionOneOneTwo(){
 };
 
 function questionOneOneOneOne(){
-	var answer = alert("As you and Dave both sit rested against\n"+
+	alert("As you and Dave both sit rested against\n"+
 		"your trees, you suddenly hear rushed footsteps as a third clown\n"+
 		"grabs Dave. You hear Dave cry for help\n"+
 		"Dave: \n"+//insert Daves help here
@@ -139,4 +139,73 @@ function questionOneOneOneTwo(){
 		"YOU WON");
 };
 
-questionOne();
+function questionOneTwo(){
+	var answer = prompt("You and Dave call an Uber you are both\n"+
+		"really starting to feel the effects of everything\n"+
+		"you did this evening. About 10 minutes later a blue van\n"+
+		"comes down the street and parks nearby. Assuming this\n"+
+		"must be the Uber you both walk up to the van. A man\n"+
+		"in his 40's rolls down the window and asks if he can help\n"+
+		"Dave replies 'Yeah we're the ones who ordered the ride'\n"+
+		"the man paused for a moment then said 'Hop in.'\n"+
+		"You give him the address and he begins driving.\n"+
+		"Dave drank a lot more than you so he fell asleep within\n"+
+		"the first 5 minutes of the drive. You stay awake and try\n"+
+		"to make small talk with the driver but you can tell he\n"+
+		"doesn't want to talk. Eventually you too fall asleep.\n"+
+		"You awake to Dave practically yelling at the guy to pull\n"+
+		"over as he's going to be sick. The man seemed reluctant\n"+
+		"until Dave threw up on him. Dave got out and went to the edge\n"+
+		"of the road, beyond it was forest. You go with him as you too could\n"+
+		"use some air. When you get next to Dave he says 'Look where we are'\n"+
+		"you look around then Dave says this is atleast 30 minutes in the\n"+
+		"opposite direction. You can either go back to the car or\n"+
+		"notion to Dave to run into the woods.\n\n"+
+		"Enter your choice: 'car' or 'woods'.").toLowerCase();
+	if(answer === "car"){
+		alert("You head back towards the car, Dave refuses to go with you.\n"+
+			"When you arrive you tell the man that your friend is too sick\n"+
+			"and you think it would be best to just walk with him home.\n"+
+			"The man looks at Dave then you, and asks you to get Dave's coat\n"+
+			"from the back. When you get in to grab the coat the man locks the\n"+
+			"doors and speeds off. You're never heard from again.\n\n"+
+			"GAME OVER");
+	}
+	else if(answer === "woods"){
+		questionOneTwoOne();
+	}
+	else{
+		alert("I didn't quite understand that try 'car' or 'woods'.");
+		questionOneTwo();
+	};
+};
+
+function questionOneTwoOne(){
+	var answer = prompt("You and Dave run into the woods a little\n"+
+		"ways then hide behind a bush. The man eventually gets impatient\n"+
+		"and goes into the woods after you. He scans over the bush you're\n"+
+		"hiding behind a couple times before going deeper into the woods.\n"+
+		"You quietly whisper with Dave and decide you should either\n"+
+		"run back to the road to look for help or stay hidden.\n\n"+
+		"Enter your choice: 'hide' or 'road'.").toLowerCase();
+	if(answer === "hide"){
+		alert("You and Dave remain hidden, after about 15 minutes\n"+
+			"a car pulls up behind the van and two men get out. \n"+
+			"The men wander into the woods with flashlights, one of\n"+
+			"them spots you both and calls for the rest. They knocked you\n"+
+			"and Dave unconcious with flashlights. Neither of you were\n"+
+			"heard from again.\n\n"+
+			"GAME OVER");
+	}
+	else if(answer === "road"){
+		alert("You both run to the road and run out in front of a\n"+
+			"passing car to get it to stop. You frantically beg them for a ride\n"+
+			"they reluctantly agree. You get in and tell them the story.\n"+
+			"They feel so bad for you both that they drive you home safely.\n\n"+
+			"YOU WON");
+	}
+	else{
+		alert("I didn't quite understand that try 'hide' or 'road'.");
+		questionOneTwoOne();
+	};
+};
